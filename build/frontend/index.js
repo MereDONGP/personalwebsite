535e33f96d120539415a54eeaafe15c24d6ce831
Don <pullend15@gmail.com>
2024-11-04 22:05:05 -0500

    adding in the final touches

--- build/frontend/index.js
+++ build/frontend/index.js
@@ -4,24 +4,60 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-import { html, css, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-let SimpleGreeting = class SimpleGreeting extends LitElement {
+let something = 0;
+let MyElement = class MyElement extends LitElement {
     constructor() {
         super(...arguments);
-        this.name = 'Somebody';
+        this.number1 = Math.floor(Math.random() * 10);
+        this.number2 = Math.floor(Math.random() * 10);
+        this.currentValue = 0;
+        this.score = 0;
     }
     render() {
-        return html `<p>Hello, ${this.name}!</p>`;
+        return html `
+     <p>Score ${this.score}</p>
+      <p>${this.number1} + ${this.number2}</p>
+      <form @submit="${this.handleSubmit}"> 
+        <p>Submit Your Answer Here <input .value=${this.currentValue} @input=${this.handleChange}> </input></p>
+        <button type="submit">Submit your answer here</button>
+      </form>
+    `;
+    }
+    getSum() {
+        return this.number1 + this.number2;
+    }
+    handleChange(event) {
+        const target = event.target;
+        this.currentValue = Number(target.value);
+        console.log(this.currentValue);
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        if (this.currentValue === this.getSum()) {
+            this.score++;
+            this.number1 = Math.floor(Math.random() * 10);
+            this.number2 = Math.floor(Math.random() * 10);
+        }
+        else {
+            this.score--;
+        }
     }
 };
-SimpleGreeting.styles = css `
-    p { color: blue; }
-  `;
 __decorate([
-    property({ type: String })
-], SimpleGreeting.prototype, "name", void 0);
-SimpleGreeting = __decorate([
-    customElement('simple-greeting')
-], SimpleGreeting);
-export { SimpleGreeting };
+    property({ type: Number })
+], MyElement.prototype, "number1", void 0);
+__decorate([
+    property({ type: Number })
+], MyElement.prototype, "number2", void 0);
+__decorate([
+    property({ type: Number })
+], MyElement.prototype, "currentValue", void 0);
+__decorate([
+    property({ type: Number })
+], MyElement.prototype, "score", void 0);
+MyElement = __decorate([
+    customElement("my-element")
+], MyElement);
+export { MyElement };
--- frontend/index.ts
+++ frontend/index.ts
@@ -1,16 +1,53 @@
 import { html, css, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
-@customElement('simple-greeting')
-export class SimpleGreeting extends LitElement {
-  static styles = css`
-    p { color: blue; }
-  `;
+let something : Number = 0
+@customElement("my-element")
+export class MyElement extends LitElement {
+  @property({type: Number})
+  number1 = Math.floor(Math.random() * 10)
 
-  @property({ type: String })
-  name = 'Somebody';
+  @property({type: Number})
+  number2 = Math.floor(Math.random() * 10)
 
-  render() {
-    return html`<p>Hello, ${this.name}!</p>`;
+  @property({type: Number})
+  currentValue = 0
+  
+  @property({type: Number})
+  score = 0
+
+
+  protected render(){
+     return html `
+     <p>Score ${this.score}</p>
+      <p>${this.number1} + ${this.number2}</p>
+      <form @submit="${this.handleSubmit}"> 
+        <p>Submit Your Answer Here <input .value=${this.currentValue} @input=${this.handleChange}> </input></p>
+        <button type="submit">Submit your answer here</button>
+      </form>
+    `
+  }
+
+  private getSum () : Number{
+    return this.number1 + this.number2
+  }
+
+  private handleChange(event: Event){
+
+    const target = event.target as HTMLInputElement
+    this.currentValue = Number(target.value)
+    console.log(this.currentValue)
+  }
+
+  private handleSubmit (event: Event) {
+    event.preventDefault()
+    if(this.currentValue === this.getSum()){
+      this.score++
+      this.number1 = Math.floor(Math.random() * 10)
+      this.number2 = Math.floor(Math.random() * 10)
+    }else{
+      this.score--
+    }
   }
 }
+
